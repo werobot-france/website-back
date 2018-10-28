a86e4dbed2e759516082b4ccf065b1372e400bed
Matthieu Bessat <spamfree@matthieubessat.fr>
2018-10-28 15:48:32 +0100

    fix(CORS): add DELETE method to allowed methods

--- src/Controllers/CORSController.php
+++ src/Controllers/CORSController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controllers;
 
-use Psr\Http\Message\ServerRequestInterface;
 use Slim\Http\Response;
 
 class CORSController extends Controller
 {
-    public function allOptions(ServerRequestInterface $request, Response $response) {
+    public function allOptions(Response $response) {
         return $response->withJson(true);
     }
 
-//    public function allOptions(ServerRequestInterface $request, Response $response) {
-//        return $response->withJson(true);
-//    }
+    public function notFound(Response $response) {
+        return $response->withJson([
+            'success' => false,
+            'errors' => ['Page not found']
+        ], 404);
+    }
 }
