d2669f2431cf74ac58c4fbf13a378911d8e7c554
Matthieu Bessat <spamfree@matthieubessat.fr>
2018-11-10 19:53:37 +0100

    fix: fix routes controller namespace

--- src/routes.php
+++ src/routes.php
@@ -6,13 +6,13 @@ $app->add(new \App\Middlewares\CORSMiddleware());
 $app->get('/', [\App\Controllers\DefaultController::class, 'home']);
 
 $app->group('/post', function () use ($app) {
-   $this->get('[/]', [\App\Controllers\Blog\PostController::class, 'getMany']);
-   $this->get('/{id}[/]', [\App\Controllers\Blog\PostController::class, 'getOne']);
-   $this->post('[/]', [\App\Controllers\Blog\PostController::class, 'store'])
+   $this->get('[/]', [\App\Controllers\PostController::class, 'getMany']);
+   $this->get('/{id}[/]', [\App\Controllers\PostController::class, 'getOne']);
+   $this->post('[/]', [\App\Controllers\PostController::class, 'store'])
        ->add(new \App\Middlewares\JWTMiddleware($app->getContainer()));
-   $this->put('/{id}[/]', [\App\Controllers\Blog\PostController::class, 'update'])
+   $this->put('/{id}[/]', [\App\Controllers\PostController::class, 'update'])
        ->add(new \App\Middlewares\JWTMiddleware($app->getContainer()));
-   $this->delete('/{id}[/]', [\App\Controllers\Blog\PostController::class, 'destroy'])
+   $this->delete('/{id}[/]', [\App\Controllers\PostController::class, 'destroy'])
        ->add(new \App\Middlewares\JWTMiddleware($app->getContainer()));
 });
 
@@ -33,12 +33,12 @@ $app->post('/image-upload', [\App\Controllers\ImageUpload::class, 'upload'])
 
 
 $app->group('/auth', function (){
-    $this->get('/login[/]', [\App\Controllers\Account\STAILEUController::class, 'getLogin']);
+    $this->get('/login[/]', [\App\Controllers\STAILEUController::class, 'getLogin']);
 
-    $this->get('/info', [\App\Controllers\Account\STAILEUController::class, 'getInfo'])
+    $this->get('/info', [\App\Controllers\STAILEUController::class, 'getInfo'])
         ->add(new \App\Middlewares\JWTMiddleware($this->getContainer()));
 
-    $this->post('/execute[/]', [\App\Controllers\Account\STAILEUController::class, 'execute'])
+    $this->post('/execute[/]', [\App\Controllers\STAILEUController::class, 'execute'])
         ->add(new \RKA\Middleware\IpAddress());
 });
 
