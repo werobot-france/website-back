bc4f6858c48d4dd049d6a4b5bf105eda30761cbb
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-07-03 21:52:11 +0200

    fix: use instagram scrapping to get medias

--- src/Instagram.php
+++ src/Instagram.php
@@ -2,51 +2,27 @@
 
 namespace App;
 
-use GuzzleHttp\Client;
-
 class Instagram
 {
-    /**
-     * @var string
-     */
-    private $accessToken;
-
-    /**
-     * @var string
-     */
-    private $endpoint = "https://api.instagram.com/v1";
-
-    /**
-     * @var Client
-     */
-    private $client;
-
-    public function __construct(string $accessToken)
-    {
-        $this->client = new Client();
-        $this->accessToken = $accessToken;
-    }
-
     public function getMedias()
     {
-        $response = $this->client->get($this->endpoint . '/users/self/media/recent?access_token=' . $this->accessToken);
-        if ($response->getStatusCode() === 200) {
-            $data = json_decode($response->getBody()->getContents(), true);
-            return array_map(function ($item) {
-                $dateTime = (new \DateTime("@{$item['created_time']}"))->format('Y-m-d H:i:s');
-                return [
-                    'id' => $item['id'],
-                    'caption' => $item['caption']['text'],
-                    'link' => $item['link'],
-                    'images' => [
-                        'thumbnail' => $item['images']['thumbnail']['url'],
-                        'low' => $item['images']['low_resolution']['url'],
-                        'standard' => $item['images']['standard_resolution']['url'],
-                    ],
-                    'created_at' => $dateTime,
-                ];
-            }, $data['data']);
+        $instagram = new \InstagramScraper\Instagram();
+        $medias = [];
+        foreach ($instagram->getMedias('werobot') as $media) {
+            $dateTime = (new \DateTime("@{$media->getCreatedTime()}"))->format('Y-m-d H:i:s');
+            array_push($medias, [
+                'id' => $media->getId(),
+                'caption' => $media->getCaption(),
+                'link' => $media->getLink(),
+                'images' => [
+                    'thumbnail' => $media->getImageThumbnailUrl(),
+                    'low' => $media->getImageLowResolutionUrl(),
+                    'standard' => $media->getImageStandardResolutionUrl(),
+                    'high' => $media->getImageHighResolutionUrl()
+                ],
+                'created_at' => $dateTime,
+            ]);
         }
-        return [];
+        return $medias;
     }
 }
