e3e3537e10e2be5edc51b74c03a2cf0c8b23798c
Matthieu Bessat <spamfree@matthieubessat.fr>
2021-04-21 22:53:38 +0200

    fix(Photos): update instagram scrapping method

--- src/Instagram.php
+++ src/Instagram.php
@@ -25,13 +25,26 @@ class Instagram
         // }
         // return $medias;
         //die();
-        $json = file_get_contents("https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables=%7B%22id%22%3A%225408546825%22,%20%22first%22%3A%2250%22,%20%22after%22%3A%20%22%22%7D");
+        $opts = [
+            "http" => [
+                "method" => "GET",
+                "header" => "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+            ]
+        ];
+        $context = stream_context_create($opts);
+        $u = "https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables=%7B%22id%22%3A%225408546825%22,%20%22first%22%3A%2250%22,%20%22after%22%3A%20%22%22%7D";
+        $json = file_get_contents($u, false, $context);
         // $html = file_get_contents("https://www.instagram.com/werobot/");
 
         // $matches = [];
         // $re = '/window._sharedData = ({"config":[\S\s]+"});/m';
         // $doMatche = preg_match_all($re, $html, $matches, PREG_SET_ORDER, 0);
         // $json = $matches[0][1];
+        if ($json == null) {
+            $u = "https://static.werobot.fr/tmp_instagram.json";
+            $json = file_get_contents($u, false, $context);
+        }
+
         $data = json_decode($json, true);
         // $raw = ($data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']);
 
